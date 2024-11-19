0d01685c3bed6c18646355bed390cef1faa28ce7
drishya700 <110844521+drishya700@users.noreply.github.com>
2024-11-19 11:53:56 +0530

    Day 2 Commit(Frontend Enhanced and Backend Added)

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import './App.css';
+import logo from './logo.svg';
+import Navbar from './Components/Navbar';
+import Login from'./Components/Login';
+import Signup from './Components/SignUp';
+import Footer from './Components/Footer';
+import ParkingSpace from './Components/ParkingSpace';
+//import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Welcome to My React App!</h1>
-            </header>
-        </div>
-    );
+  return (
+    <div className='App'>
+      <Navbar />
+      <Login />
+      <Signup />  
+      <ParkingSpace /> 
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
